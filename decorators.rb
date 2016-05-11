fc518bc9f6c05f85f938ee16573db134aca8ecb2
pixeltot <pixeltot@github.com>
2016-05-11 15:06:25 -0700

    fixed syntax issue when calling super

--- decorators.rb
+++ decorators.rb
@@ -90,7 +90,7 @@ end
 # and ask post if it responds to the given method, otherwise, call super, which will invoke the respond_to_missing from its parent object
 
 def respond_to_missing(method_name, include_private = false)
-    post.respond_to?(method_name, include_private || super)
+    post.respond_to?(method_name, include_private) || super
 end
 
 # now, if we ask a PostDecorator if it responds to image , it'll return true
@@ -103,4 +103,100 @@ end
 # 2. "Wrap" another object
 # 3. Delegate most methods to the wrapped object.
 # 4. Provide one or two methods of their own.
+
 # ---------------------------------------------------------
+
+# Using Decorators For View Output
+
+# app\helpers\posts_helper.rb
+module PostHelper
+  def publication_date(date)
+    post.created_at.strftime '%Y-%m-%d'
+  end
+end
+
+# app\views\posts\show.html.erb
+# <span><%= publication_date @post %> </span>
+
+# here we have our PostsHelper module, with the publication date helper method
+# this method takes a post and formats the created update
+
+# a couple of issues with view helpers:
+  # 1. pollute the global namespace with methods specific to a model
+  # 2. forces a functional approach within an object oriented domain model
+
+# for example, nothing stops us here from passing a user object to the publication_date method
+# since the user object might also have a create_at view
+# so... it is still allowed to be used with other objects, but that would be wrong!
+
+# a better place for our publication_date method is inside our post_decorator.rb class
+# this way we can call publication_date as an instance method on our post decorator object, which gives
+# us a more consistent object-oriented approach throughout the view
+
+# app\decorators\post_decorator.rb
+class PostDecorator
+  def publication_date
+    post.create_at.strftime '%Y-%m-%d'
+  end
+
+  def method_missing(method_name, *args, &block)
+    '...'
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    '...'
+  end
+end
+
+# app\views\posts\show.html.erb
+# <span> <%= @post_decorator. publication_date %></span>
+
+# -------------------------------------------------------
+
+# another way we can use decorators is to output HTML
+# in this exaxmple we have a PostHelper with a post_classes method
+
+# app\helpers\post_helper.rb
+module PostHelper
+  def post_classes(post)
+    classes = ['page']
+    classes << 'front-page' if post.is_front_page?
+    classes
+  end
+end
+
+# this method takes a post and constructs a collection of CSS classes based on the post's attributes
+# then in the view, we use the output from the post_classes method to determine the classes
+
+# app\views\posts\show.html.erb
+# <article class="<%= post_classes(@post) %>">
+#   <%= @post.content %>
+# </article>
+
+# this is a great starting point but it doesnt scale well as your view specific logic grows
+# view specific logic that outputs HTML classes
+
+# ---------------------------------------------------------------
+
+# so lets move our post_classes method into our PostDecorator, and call it 'classes'
+# we dont have to pass any argument to this method
+# because we have access to the post via our attr_reader
+
+# app\decorator\post_decorator.rb
+class PostDecorator
+  attr_reader :post
+
+  def classes
+    classes = ['page', 'btn']
+    classes << 'cover' if post.is_front_page?
+    classes
+  end
+end
+
+# and in the show template, we can go back to using a more object-oriented approach
+# by calling the classes method on the post_decorator
+
+# app\view\posts\show.html.erb
+# <article class="<%= @post_decorator.classes %>">
+#   <%= @post_decorator.content %>
+# </article>
