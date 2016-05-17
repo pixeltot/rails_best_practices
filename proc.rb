629d9fa666632b86eba08ec8a71ac18ad5dd50c1
pixeltot <pixeltot@github.com>
2016-05-16 19:12:31 -0700

    how to configure your proc file and use foreman

--- proc.rb
+++ proc.rb
@@ -10,7 +10,7 @@ web: bundle exec rails s -p $PORT
 web: bundle exec rails s -p $PORT
 worker: bundle exec rake worker
 urgentworker: bundle exec rake urgent_worker
-scheduler: bundler exec rake scheduler
+scheduler: bundle exec rake scheduler
 
 # Foreman is a cmd line tool for running proc file backed apps
 $ gem install foreman
