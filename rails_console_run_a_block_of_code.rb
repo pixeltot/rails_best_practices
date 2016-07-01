fce438ff35a3af44cb4540146e1e304357dcaf09
pixeltot <pixeltot@github.com>
2016-06-30 20:51:35 -0700

    how to run a block of code in rails console

---
+++ rails_console_run_a_block_of_code.rb
@@ -0,0 +1,6 @@
+$rails c
+Loading development environment (Rails 3.2.13)
+2.0.0p0 :001 > Product.all.each do |product| #enter
+2.0.0p0 :002 >     puts product #enter
+2.0.0p0 :003?> end #enter
+# here comes the output
